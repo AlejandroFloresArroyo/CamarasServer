40516e230dc3aeb2064d2688923af38d6d4d67dc
Alejandro de Jesús Flores Arroyo <itt.alejandro.flores@hotmail.com>
2021-02-28 00:34:15 -0600

    Eliminadas clases por defecto, agregadas entidades banda y foto, se anade el DBContext y un controlador para banda

---
+++ CamKyscn/Entities/Banda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CamKyscn.Entities
+{
+	public class Banda
+	{
+		public long Id { get; set; }
+		public string Codigo { get; set; }
+
+	}
+}
