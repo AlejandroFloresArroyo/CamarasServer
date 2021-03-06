7a60dfbd41d925cb61324a3a88cfe97de07bdb7a
Alejandro de Jesus Flores Arroyo <itt.alejandro.flores@hotmail.com>
2021-03-05 21:27:40 -0600

    Eliminadas migraciones, agregados controllers y servicios de otras entidades

--- CamKyscn/Controllers/FotoController.cs
+++ CamKyscn/Controllers/FotoController.cs
@@ -1,7 +1,32 @@
+using System.Threading.Tasks;
+using CamKyscn.Services.FotoService;
+using Microsoft.AspNetCore.Mvc;
+
 namespace CamKyscn.Controllers
 {
-    public class FotoController
+    [Route("api/[controller]")]
+    [Controller]
+    public class FotoController : ControllerBase
     {
+        private readonly IFotoService _fotoService;
+        public FotoController(IFotoService fotoService)
+        {
+            this._fotoService =  fotoService;
+        }
+
+        //TODO: Generar GetAll
+        [HttpGet("{PaqueteID}")]
+        public async Task<IActionResult> GetAllByPaquete(long PaqueteID)
+        {
+            return Ok();
+        }
+
+        //TODO: Generar GetAll
+        [HttpPost]
+        public async Task<IActionResult> Add()
+        {
+            return Ok();
+        }
         
     }
 }
--- CamKyscn/Entities/Banda.cs
+++ CamKyscn/Entities/Banda.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 
 namespace CamKyscn.Entities
 {
-	public class Banda
-	{
-		public long Id { get; set; }
-		public string Codigo { get; set; }
+    public class Banda
+    {
+        public long Id { get; set; }
+        public string Codigo { get; set; }
+        public long? PaqueteId { get; set; }
 
-	}
+    }
 }
